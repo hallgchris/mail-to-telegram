3c6c3b94b7269da6a975632718e53a3425dbe82c
Christopher Hall <hallgchris@gmail.com>
2021-06-18 14:11:34 +1000

    fw: Don't send multiple notifications if switch is held open

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -19,9 +19,17 @@ static const float LOW_BATTERY_THRESHOLD = 1.2 * 3; // Volts, 3 cells at 1.2V
 WiFiClientSecure secured_client;
 UniversalTelegramBot bot(BOT_TOKEN, secured_client);
 
+enum LetterboxState
+{
+  Open,
+  Closed
+};
+
+RTC_DATA_ATTR LetterboxState letterbox_state = Open;
+
 void configure_wifi()
 {
-  // attempt to connect to Wifi network:
+  // Attempt to connect to Wifi network:
   Serial.print("Connecting to Wifi SSID ");
   Serial.print(WIFI_SSID);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -35,7 +43,7 @@ void configure_wifi()
   Serial.println(WiFi.localIP());
 
   Serial.print("Retrieving time: ");
-  configTime(0, 0, "pool.ntp.org"); // get UTC time via NTP
+  configTime(0, 0, "pool.ntp.org"); // Get UTC time via NTP
   time_t now = time(nullptr);
   while (now < 24 * 3600)
   {
@@ -73,10 +81,44 @@ void print_wakeup_reason(const esp_sleep_wakeup_cause_t wakeup_reason)
 
 float read_battery_voltage()
 {
-  const int r1 = 100;
-  const int r2 = 100;
+  const int r1 = 1;
+  const int r2 = 1;
   const float divider_factor = (float)r1 / (r1 + r2);
   return (float)analogReadMilliVolts(BAT_PIN) / divider_factor / 1000;
+}
+
+void on_touchpad_triggered()
+{
+  Serial.println("Triggered by touchpad: sending battery voltage");
+  configure_wifi();
+  const float battery_voltage = read_battery_voltage();
+  bot.sendMessage(CHAT_ID, "Battery voltage is " + String(battery_voltage) + " V");
+  Serial.println("Message sent!");
+}
+
+void on_reed_triggered()
+{
+  Serial.print("Triggered by EXT1: ");
+
+  if (letterbox_state == Closed)
+  {
+    Serial.println("Letterbox opened, sending message and waiting to go high");
+    letterbox_state = Open;
+
+    configure_wifi();
+    const float battery_voltage = read_battery_voltage();
+
+    String message = "We just got a letter!";
+    if (battery_voltage <= LOW_BATTERY_THRESHOLD)
+      message += " Battery low (" + String(battery_voltage) + " V)";
+    bot.sendMessage(CHAT_ID, message);
+    Serial.println("Message sent!");
+  }
+  else
+  {
+    Serial.println("Letterbox closed, ready for next message");
+    letterbox_state = Closed;
+  }
 }
 
 void setup()
@@ -88,23 +130,15 @@ void setup()
   Serial.begin(115200);
   Serial.println("Starting...");
 
-  const float battery_voltage = read_battery_voltage();
-
   const auto wakeup_reason = esp_sleep_get_wakeup_cause();
   print_wakeup_reason(wakeup_reason);
-  String message;
   switch (wakeup_reason)
   {
   case ESP_SLEEP_WAKEUP_TOUCHPAD:
-    configure_wifi();
-    bot.sendMessage(CHAT_ID, "Battery voltage is " + String(battery_voltage) + " V");
+    on_touchpad_triggered();
     break;
   case ESP_SLEEP_WAKEUP_EXT1:
-    configure_wifi();
-    message = "We just got a letter!";
-    if (battery_voltage <= LOW_BATTERY_THRESHOLD)
-      message += " Battery low (" + String(battery_voltage) + " V)";
-    bot.sendMessage(CHAT_ID, message);
+    on_reed_triggered();
     break;
   default:
     break;
@@ -113,8 +147,9 @@ void setup()
   touchAttachInterrupt(TOUCH_PIN, nullptr, 40);
   esp_sleep_enable_touchpad_wakeup();
 
-  esp_sleep_enable_ext1_wakeup(REED_PIN_MASK, ESP_EXT1_WAKEUP_ALL_LOW);
+  esp_sleep_enable_ext1_wakeup(REED_PIN_MASK, letterbox_state == Open ? ESP_EXT1_WAKEUP_ANY_HIGH : ESP_EXT1_WAKEUP_ALL_LOW);
 
+  Serial.println("Entering deep sleep");
   esp_deep_sleep_start();
 }
 
